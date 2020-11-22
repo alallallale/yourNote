068544c4c70af02947e05ec2a3ee58b28fb67864
jnh304 <jnh304@gmail.com>
2020-11-22 20:33:06 +0900

    linux에 있는 기본 설정spring

---
+++ src/main/java/jo/project/yourNote/YourNoteApplication.java
@@ -0,0 +1,13 @@
+package jo.project.yourNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jo/project/yourNote/YourNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package jo.project.yourNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
